94df6a55ee0907c6f254897ad9b3adf3aa29c883
benha <ben.halfon@s.afeka.ac.il>
2020-03-30 12:48:50 +0300

    before testing sprint2 ready for postman

---
+++ src/main/java/demo/ActionController.java
@@ -0,0 +1,24 @@
+package demo;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ActionBoundaryClasses.ActionBoundary;
+
+
+@RestController
+public class ActionController {
+	@RequestMapping(path="/acs/actions",
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String,Object> createNewUser(@RequestBody  ActionBoundary user) {
+		return Collections.singletonMap("type", "blabla");
+	}
+}
