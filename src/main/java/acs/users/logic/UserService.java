0933036cae262d29c4219ae1894fd0a74c29e471
majdi2 <majdi2@bitbucket.com>
2020-04-06 13:48:11 +0300

    UserService interface

---
+++ src/main/java/acs/users/logic/UserService.java
@@ -0,0 +1,13 @@
+package acs.users.logic;
+import java.util.List;
+
+import acs.users.UserBoundary;
+
+public interface UserService {
+	public UserBoundary	createUser(UserBoundary user);
+	public UserBoundary	login(String userDomain, String userEmail);
+	public UserBoundary	updateUser(String userDomain, String userEmail, UserBoundary update);
+	public List<UserBoundary> getAllUsers(String adminDomain, String adminEmail);
+	public void	deleteAllUsers(String adminDomain, String adminEmail);	
+	
+}
